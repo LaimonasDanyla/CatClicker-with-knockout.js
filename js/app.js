c5fc85b59128c3c85b6a6ed0072f0799b3244913
Laimonas Danyla <laimonas.danyla@gmail.com>
2016-11-01 22:11:01 +0200

    refactor: Separate viewmodel functions to make code more readable. Change the array of nicknames as  in lesson and display nickname data accordingly

--- js/app.js
+++ js/app.js
@@ -1,35 +1,34 @@
-var ViewModel = function() {
-    this.clickCount = ko.observable(0);
-    //this.name = ko.observable('Tabby');
-    this.cats = ko.observableArray([
-      { name: 'Kitty' },
-      { name: 'Kit-teacup'},
-      { name: 'Kittin around'},
-      { name: 'Kittles'},
-      { name: 'Kitty Kitty'}
-    ]);
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('www.flickr.com');
+var Cat = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imgAttribution = ko.observable('www.flickr.com');
 
-    this.incrementCounter = function() {
-      this.clickCount(this.clickCount() + 1);
-    };
-
-    this.catLevel = ko.computed(function() {
-      if (this.clickCount() <= 10) {
-        return "Click level: Small";
-      } else if (this.clickCount() >= 11 && this.clickCount() <= 20) {
-        return "Click level: Medium";
-      } else {
-        return "Click level: Big";
-      }
-    }, this);
-
-
-    this.addCat = function() {
-      self.cats.push({ name });
+  this.catLevel = ko.computed(function() {
+    var clicks = this.clickCount();
+    if (clicks <= 10) {
+      return "Click level: Low";
+    } else if (clicks >= 11 && clicks <= 20) {
+      return "Click level: Medium";
+    } else {
+      return "Click level: High";
     }
+  }, this);
 
+  this.nickNames = ko.observableArray(['Kitty', 'Kit-teacup', 'Kittin around', 'Kittles', 'Kitty Kitty']);
+  this.addCat = function() {
+    this.nickNames.push( $data );
+  };
+}
+//this was causing trouble - bindings are applied via ViewModel
+//ko.applyBindings(new Cat());
+
+
+var ViewModel = function() {
+  this.currentCat = ko.observable ( new Cat() );
+  this.incrementCounter = function() {
+      this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
 }
 
 ko.applyBindings(new ViewModel());
