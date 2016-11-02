0f6208670690aec42afc5a6eca43b4609c975f85
Laimonas Danyla <laimonas.danyla@gmail.com>
2016-11-02 22:22:52 +0200

    refactor: move Cat() data to ViewModel, adjust Cat() function accordingly

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imgAttribution = ko.observable('www.flickr.com');
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
 
   this.catLevel = ko.computed(function() {
     var clicks = this.clickCount();
@@ -38,7 +38,13 @@ var ViewModel = function() {
 var ViewModel = function() {
   var self = this; // this is a trick to access inner this and not to confuse
   // with outer this.
-  this.currentCat = ko.observable( new Cat() ); // outer this
+  this.currentCat = ko.observable( new Cat({
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'www.flickr.com/photos/bigtallguy/434164568',
+    nickNames: ['Kitty', 'Kit-teacup', 'Kittin around', 'Kittles', 'Kitty Kitty']
+  }) );
   this.incrementCounter = function() {
     self.currentCat().clickCount(self.currentCat().clickCount() + 1);
   }
